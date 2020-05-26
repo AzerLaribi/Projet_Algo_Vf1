9b6e3c68ff494c534e088ce3dd9326b84bf1d1c0
unknown <azer2017laribi@gmail.com>
2020-05-26 14:09:03 +0100

    Azer

---
+++ src/taquin/Musique.java
@@ -0,0 +1,46 @@
+package taquin;
+
+
+import java.io.*;
+import javazoom.jl.player.*;
+
+public class Musique extends Thread {
+
+    private String fileLocation;
+    private boolean loop;
+    private Player player;
+    private int pauseOnFrame = 0;
+    
+    /**
+     * Constructor
+     * @param fileLocation
+     * @param loop 
+     */
+    public Musique(String fileLocation, boolean loop) {
+        this.fileLocation = fileLocation;
+        this.loop = loop;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            do {
+                FileInputStream buff = new FileInputStream(fileLocation);
+                player = new Player(buff);
+                player.play();
+            } while (loop);
+        } catch (Exception ioe) {
+        	System.out.println(ioe.toString());
+        }
+    }
+    
+    /**
+     * Close method
+     */
+    public void close(){
+        loop = false;
+        player.close();
+        this.interrupt();
+    }
+}
